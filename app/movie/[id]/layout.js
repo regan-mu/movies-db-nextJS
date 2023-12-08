0197f1d5cefd6169213a589005a358d721516594
regan-mu <regansomi@gmail.com>
2023-12-09 02:01:36 +0300

    Add mobile responsiveness in movie page

--- app/movie/[id]/layout.js
+++ app/movie/[id]/layout.js
@@ -17,16 +17,16 @@ async function Movie ({params, children}) {
     const data = await fetchData(params.id);
     return (
         <div className="min-h-screen p-10">
-            <div className="h-[40vh] relative">
+            <div className="h-[30vh] sm:h-[40vh] relative">
                 <Image 
                     src={`https://image.tmdb.org/t/p/original/${data.poster_path}`} alt="movie banner" 
                     fill
                     className="object-cover rounded-lg w-full"
                 />
             </div>
-            <h1 className="text-4xl font-bold text-center pt-5">{data.title}</h1>
-            <div className="flex gap-x-10 mt-10">
-                <div className="w-1/2 font-medium">
+            <h1 className="text-2xl sm:text-4xl font-bold text-center pt-5">{data.title}</h1>
+            <div className="flex gap-y-5 sm:gap-x-10 mt-2 sm:mt-10 flex-col sm:flex-row" >
+                <div className="w-full font-medium sm:w-1/2">
                     <h1>
                         <span className="underline">Homepage:</span> <Link href={data.homepage} target="_blank">Link</Link>
                     </h1>
@@ -36,7 +36,7 @@ async function Movie ({params, children}) {
                     <p><span className="underline">Overview:</span> {data.overview}</p>
                     <p><span className="underline">Release Date:</span> {data.release_date}</p>
                 </div>
-                <div className="w-1/2">
+                <div className="w-full sm:w-1/2">
                     {children}
                 </div>
             </div>
